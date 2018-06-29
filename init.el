2c5e2a8aa60147a6cf5e253673d98ff6cb307dae
blak3mill3r <blak3mill3r@gmail.com>
2018-06-29 21:58:36 +0000

    remove encrypted blake stuff

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/cfg/")
 
-(load "cfg-sensitive.el")
+;; (load "cfg-sensitive.el")
 (load "cfg-read-from-pipe.el")
 (load "cfg-prelude.el")
 (load "cfg-documentation.el")
